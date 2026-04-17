2bf5f211d181c20386053185f3ed92b277cc9cf0
LAGx <kvarkmail@gmail.com>
2026-04-17 06:11:38 +0300

    new design of crates

--- src/engine/logical_scene/ph_crates_scene.cpp
+++ src/engine/logical_scene/ph_crates_scene.cpp
@@ -22,19 +22,20 @@ ray_error ph_crates_scene::init(window& win, pipeline_manager& pipe) {
 
         world_processor.register_pipelines(crate_sim.get_pipelines());
 
-        crate_sim.add_crate(glm::vec2(-180, -55), 50, 'r', glm::vec4(102.f, 153.f, 51.f, 255.f) / 255.f, glm::vec4(128.f, 93.f, 21.f, 255.f) / 255.f);
-        crate_sim.add_crate(glm::vec2(-120, -55), 50, 'a', glm::vec4(144.f, 191.f, 96.f, 255.f) / 255.f, glm::vec4(18.f, 54.f, 82.f, 255.f) / 255.f);
-        crate_sim.add_crate(glm::vec2(-60, -55), 50, 'y', glm::vec4(67.f, 115.f, 51.f, 19.f) / 255.f, glm::vec4(212.f, 178.f, 106.f, 255.f) / 255.f);
-        crate_sim.add_crate(glm::vec2(0, -55), 50, 'p', glm::vec4(192.f, 230.f, 153.f, 255.f) / 255.f, glm::vec4(41.f, 79.f, 109.f, 255.f) / 255.f);
-        crate_sim.add_crate(glm::vec2(60, -55), 50, 'a', glm::vec4(36.f, 96.f, 104.f, 255.f) / 255.f, glm::vec4(134.f, 161.f, 54.f, 255.f) / 255.f);
-        crate_sim.add_crate(glm::vec2(120, -55), 50, 't', glm::vec4(192.f, 230.f, 153.f, 255.f) / 255.f, glm::vec4(41.f, 79.f, 109.f, 255.f) / 255.f);
-        crate_sim.add_crate(glm::vec2(180, -55), 50, 'h', glm::vec4(36.f, 96.f, 104.f, 255.f) / 255.f, glm::vec4(134.f, 161.f, 54.f, 255.f) / 255.f);
-        crate_sim.add_crate(glm::vec2(-180, 10), 50, 'c', glm::vec4(105.f, 150.f, 156.f, 255.f) / 255.f, glm::vec4(176.f, 202.f, 101.f, 255.f) / 255.f);
-        crate_sim.add_crate(glm::vec2(-120, 10), 50, 'r', glm::vec4(66.f, 122.f, 130.f, 255.f) / 255.f, glm::vec4(95.f, 121.f, 20.f, 255.f) / 255.f);
-        crate_sim.add_crate(glm::vec2(-60, 10), 50, 'a', glm::vec4(36.f, 96.f, 104.f, 255.f) / 255.f, glm::vec4(134.f, 161.f, 54.f, 255.f) / 255.f);
-        crate_sim.add_crate(glm::vec2(0, 10), 50, 't', glm::vec4(66.f, 122.f, 130.f, 255.f) / 255.f, glm::vec4(95.f, 121.f, 20.f, 255.f) / 255.f);
-        crate_sim.add_crate(glm::vec2(60, 10), 50, 'e', glm::vec4(36.f, 96.f, 104.f, 255.f) / 255.f, glm::vec4(134.f, 161.f, 54.f, 255.f) / 255.f);
-        crate_sim.add_crate(glm::vec2(120, 10), 50, 's', glm::vec4(66.f, 122.f, 130.f, 255.f) / 255.f, glm::vec4(95.f, 121.f, 20.f, 255.f) / 255.f);
+        // Neon Cyberpunk (session_17820012)
+        crate_sim.add_crate(glm::vec2(-180, -55), 50, 'r', glm::vec4(15.f, 5.f, 30.f, 255.f) / 255.f, glm::vec4(255.f, 0.f, 200.f, 255.f) / 255.f);
+        crate_sim.add_crate(glm::vec2(-120, -55), 50, 'a', glm::vec4(5.f, 20.f, 10.f, 255.f) / 255.f, glm::vec4(0.f, 255.f, 100.f, 255.f) / 255.f);
+        crate_sim.add_crate(glm::vec2(-60, -55), 50, 'y', glm::vec4(20.f, 0.f, 40.f, 255.f) / 255.f, glm::vec4(200.f, 0.f, 255.f, 255.f) / 255.f);
+        crate_sim.add_crate(glm::vec2(0, -55), 50, 'p', glm::vec4(0.f, 20.f, 30.f, 255.f) / 255.f, glm::vec4(0.f, 230.f, 255.f, 255.f) / 255.f);
+        crate_sim.add_crate(glm::vec2(60, -55), 50, 'a', glm::vec4(30.f, 10.f, 0.f, 255.f) / 255.f, glm::vec4(255.f, 180.f, 0.f, 255.f) / 255.f);
+        crate_sim.add_crate(glm::vec2(120, -55), 50, 't', glm::vec4(10.f, 0.f, 25.f, 255.f) / 255.f, glm::vec4(255.f, 50.f, 180.f, 255.f) / 255.f);
+        crate_sim.add_crate(glm::vec2(180, -55), 50, 'h', glm::vec4(0.f, 15.f, 5.f, 255.f) / 255.f, glm::vec4(50.f, 255.f, 80.f, 255.f) / 255.f);
+        crate_sim.add_crate(glm::vec2(-180, 10), 50, 'c', glm::vec4(0.f, 5.f, 30.f, 255.f) / 255.f, glm::vec4(0.f, 200.f, 255.f, 255.f) / 255.f);
+        crate_sim.add_crate(glm::vec2(-120, 10), 50, 'r', glm::vec4(25.f, 0.f, 35.f, 255.f) / 255.f, glm::vec4(255.f, 20.f, 220.f, 255.f) / 255.f);
+        crate_sim.add_crate(glm::vec2(-60, 10), 50, 'a', glm::vec4(5.f, 25.f, 5.f, 255.f) / 255.f, glm::vec4(20.f, 255.f, 120.f, 255.f) / 255.f);
+        crate_sim.add_crate(glm::vec2(0, 10), 50, 't', glm::vec4(0.f, 10.f, 25.f, 255.f) / 255.f, glm::vec4(0.f, 220.f, 240.f, 255.f) / 255.f);
+        crate_sim.add_crate(glm::vec2(60, 10), 50, 'e', glm::vec4(28.f, 5.f, 0.f, 255.f) / 255.f, glm::vec4(255.f, 200.f, 10.f, 255.f) / 255.f);
+        crate_sim.add_crate(glm::vec2(120, 10), 50, 's', glm::vec4(15.f, 0.f, 28.f, 255.f) / 255.f, glm::vec4(210.f, 0.f, 255.f, 255.f) / 255.f);
 
         return {};
 }
